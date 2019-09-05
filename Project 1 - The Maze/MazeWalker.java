c1e4ed19f2b86f543f8f508074326b02319390fe
799761 <54371387+799761@users.noreply.github.com>
2019-09-05 10:20:18 -0700

    Add files via upload
    maze code

--- Project 1 - The Maze/MazeWalker.java
+++ Project 1 - The Maze/MazeWalker.java
@@ -9,8 +9,7 @@ public class MazeWalker
 {
     // instance variables - replace the example below with your own
     private int x;
-
-    /**
+/**
      * Constructor for objects of class MazeWalker
      */
     public MazeWalker()
@@ -19,16 +18,29 @@ public class MazeWalker
         x = 0;
         
     }
-
+    
     /**
      * An example of a method - replace this comment with your own
      *
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public void walkMaze(Maze maze, MazeBot mazeBot)
-    {
-        
+    public void walkMaze(Maze maze, MazeBot mazeBot){
+    // loops
+    while(mazeBot.didReachGoal() == false){
+            while(mazeBot.canMoveForward())
+                mazeBot.moveForward();
+        }
+    // if it can't go forward it'll check where it can go
+        if(mazeBot.canMoveForward() == false){
+           mazeBot.turnRight();
+        if(mazeBot.canMoveForward()){      
+            mazeBot.moveForward();
+        }
+        else{
+             mazeBot.turnLeft();
+             mazeBot.turnLeft();
+            }
         // put your code here
         mazeBot.moveForward();
         mazeBot.moveForward();
@@ -44,24 +56,22 @@ public class MazeWalker
         mazeBot.moveForward();
         mazeBot.moveForward();
         mazeBot.moveForward();
-        
         mazeBot.turnRight();
         mazeBot.moveForward();
         mazeBot.moveForward();
         mazeBot.moveForward();
         mazeBot.turnRight();
         mazeBot.moveForward();
-        
         mazeBot.moveForward();
         mazeBot.turnLeft();
         mazeBot.moveForward();
         mazeBot.moveForward();
-        
-       
-      
-      
-      
-        
-     
+        if (mazeBot.didReachGoal()){
+            mazeBot.signalSuccess();
+        }
+        else {
+             mazeBot.signalError();
+        }
     }
-}
+}  
+}     
