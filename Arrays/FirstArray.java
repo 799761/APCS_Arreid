4231bb71f078a57094f8bbec8cecf2ad8dbce059
799761 <54371387+799761@users.noreply.github.com>
2019-09-10 10:27:46 -0700

    Add files via upload
    Arrays project

---
+++ Arrays/FirstArray.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class FirstArray here.
+ *
+ * @author (rreid)
+ * @version (9/10)
+ */
+public class FirstArray
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class FirstArray
+     */
+    public void loadArray(){
+    {
+        // initialise instance variables
+        x = 10;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    int[] nums = new int[100];
+    {
+        // put your code here
+        
+    }
+}
+}
