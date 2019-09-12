86f8696800f903ff49f0676548caa88ca9e778c1
799761 <54371387+799761@users.noreply.github.com>
2019-09-12 10:19:49 -0700

    Add files via upload
    DigitArray lab 9/12 b

---
+++ Lab912b DigitArray/DigitArray.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class DigitArray here.
+ *
+ * @author (rreid)
+ * @version (912)
+ */
+public class DigitArray
+{
+    // instance variables - replace the example below with your own
+    private int num1;
+    private int[] digits;
+
+    /**
+     * Constructor for objects of class DigitArray
+     */
+    public DigitArray()
+    {
+        // initialise instance variables
+        num1 = 0;
+        int[] digits;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void loadArray(num);
+    {
+        // put your code here
+        return x + y;
+    }
+}
