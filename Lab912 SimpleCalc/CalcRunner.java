695234c7f59b712b8de90f3756f72b8b9cbcf56e
799761 <54371387+799761@users.noreply.github.com>
2019-09-12 10:18:00 -0700

    Add files via upload

---
+++ Lab912 SimpleCalc/CalcRunner.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class CalcRunner here.
+ *
+ * @author (rreid)
+ * @version (912a)
+ */
+public class CalcRunner{
+// instance variables - replace the example below with your own
+    simpleCalc int;
+// constructor
+/**
+     * Constructor for objects of class CalcRunner
+     */
+    public CalcRunner()
+    {
+        // initialise instance variables
+        calcRunner ;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int add(int x, int y){
+    {
+        // put your code here
+        
+    }
+}
+}
