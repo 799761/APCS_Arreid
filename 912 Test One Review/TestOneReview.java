2e689f4c6ff144efb7379c7358df8870fcc2d38d
799761 <54371387+799761@users.noreply.github.com>
2019-09-16 10:25:34 -0700

    Add files via upload
    test 1 review work

---
+++ 912 Test One Review/TestOneReview.java
@@ -0,0 +1,63 @@
+
+/**
+ * Write a description of class TestOneReview here.
+ *
+ * @author (rreid)
+ * @version (912)
+ */
+public class TestOneReview
+{
+    // declare and initialize int, double, boolen
+    boolean test = 7;
+    
+  
+    // given a class(w or w/o paramters) declare & initialize object for that class
+    
+    // create pumpkin object with an initial value of 3
+    
+    // write a class with instance variables, constructor and methods
+    
+    // use integer division and modular division to manipulate the digits of an integer
+    
+    // create a sum method that will return the sum of the digits in an integer of one or more digits
+    
+    // create simple calc methods such as add, multiply, divide and subtract that will take in 2 integers and return the appropriate value
+    
+    //show output values for a while loop
+    
+    // understand if/if-else statements 
+    
+    //declare/initialize an Array of random integers named "nums" use for loop to initialize
+    
+    // use for loop to traverse and array
+    
+    //use for loop to find sum of values in an array
+    
+    //copy values of one array into another
+
+    /**
+     * Constructor for objects of class TestOneReview
+     */
+    public TestOneReview()
+    {
+        // # 1
+        if (test == 7){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
