6218309a1cffabfc19ec886c19073ab611aa456a
799761 <54371387+799761@users.noreply.github.com>
2019-09-20 10:22:52 -0700

    Add files via upload
    sentence builder 920 lab

---
+++ SentenceBuilder/BuildSentences.java
@@ -0,0 +1,88 @@
+
+/**
+ * Write a description of class BuildSentences here.
+ *
+ * @author (rreid)
+ * @version (920)
+ */
+public class BuildSentences
+{
+    // instance variables - replace the example below with your 
+    String[] Nouns = ("snake") , "banana" , "golden retriever" , "apple" , "NYC" , "man");
+    String[] Verbs = ("flies" "soars" , "gallops" , "hits" , "walks" , "skips");
+    String[] Adjectives = ("fuzzy" , "long" , "sticky" , "happy" , "tired" , "grateful");
+    String[] Determiners = ("hers" , "your" , "Sara's " , "my" , "their" , "our");
+    String[] Adverbs = ("smoothly" , "angrily" , "helplessly" , "joyfully" , "never" , "always");
+    String[] Prepositions = ("near" , "around" , "on top of" , "behind" , "inside of" , "between");
+ 
+    /**
+     * Constructor for objects of class BuildSentences
+     */
+    public BuildSentences()
+    {
+        // initialise instance variables
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void loadSentences()
+    {
+        // put your code here
+    }
+    
+    public String getSentence()
+    {
+        // code here
+    }
+    
+    public String getNounPhrase()
+    {
+        // code here
+    }
+    
+    public String getPrepositionalPhrase()
+    {
+        // code here
+    }
+    
+    public String getVerbPhrase()
+    {
+        // code here
+    }
+    
+    public String getRandomNoun()
+    {
+        // code
+    }
+    
+    public String getRandomVerb()
+    {
+        // code here
+    }
+    
+    public String getRandomAdjective()
+    {
+        // code here
+    }
+    
+    public String getRandomAdverb()
+    {
+        // code here
+    }
+    
+    public String getRandomDeterminer()
+    {
+        // code here
+    }
+    
+    public String getRandomPreposition()
+    {
+        // code here
+    }
+}
+
+
