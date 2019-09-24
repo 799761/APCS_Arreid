61e790558974e0736891b517bfc2b1efc753086b
799761 <54371387+799761@users.noreply.github.com>
2019-09-24 10:26:20 -0700

    Add files via upload
    name parser lab 9/24

---
+++ NameParser/Runner.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Runner here.
+ *
+ * @author (rreid)
+ * @version (924)
+ */
+public class Runner
+{
+    // instance variables - replace the example below with your own
+    String FName = " ";
+    String LName = " ";
+    
+    
+
+    /**
+     * Constructor for objects of class Runner
+     */
+    public Runner()
+    {
+        // initialise objects
+        Runner nameOne = new Object(twh
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+     public int getName(){    
+        // put your code here
+        FName = str.substring(0, si);
+        LName = str.substring(si = 1);
+        
+        
+    }
+}
