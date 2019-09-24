22a1bd9e1c152c299153d51c7708a04309503594
799761 <54371387+799761@users.noreply.github.com>
2019-09-24 10:27:00 -0700

    Add files via upload
    nested loops lab

---
+++ NestedLoops/Loops.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Loops here.
+ *
+ * @author (rreid)
+ * @version (918)
+ */
+public class Loops
+{
+    // instance variables - replace the example below with your own
+    public static void main(){
+        problemA();
+    }
+
+    /**
+     * Constructor for objects of class Loops
+     */
+    public static void problemA()
+    {
+        // initialise instance variables
+        for(int n = 2; n <= 6; n++){
+            // code for this loop
+            for(int n = 0; n <= 6; n++){
+                System.out.println(n);
+            }
+            System.out.println();
+        }
+   
+   
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+    }
+}
+
