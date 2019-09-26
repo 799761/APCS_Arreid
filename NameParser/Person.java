64db730d18574bda808e9bbcb0c15e8ead8c3b3b
Roxy R <rr@WS-ACADBUS.EDU>
2019-09-26 13:43:30 -0700

    926

--- NameParser/Person.java
+++ NameParser/Person.java
@@ -2,32 +2,34 @@
 /**
  * Write a description of class Person here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (rreid)
+ * @version (924/926)
  */
 public class Person
 {
     // instance variables - replace the example below with your own
-    private int x;
+    String fName;
+    String mName;
+    String lName;
 
     /**
      * Constructor for objects of class Person
      */
     public Person()
     {
-        // initialise instance variables
-        x = 0;
+       fName = parser.next();
+            mName = parser.next();
+            if (parser.hasNext()){
+                lName = parser.next();
+            } else {
+                lName = mName;
+                mName = " ";
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   
 }
+
+
+}
+
+
---
+++ NameParser/PersonRunner.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Runner here.
+ *
+ * @author (rreid)
+ * @version (924/926)
+ */
+import java.util.Scanner;
+public class PersonRunner
+{
+    // instance variables - replace the example below with your own
+   
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void main(){
+        Person person = new Person(); // while loop
+        // put your code here
+       
+            Scanner parser = new Scanner(fullName);
+            
+    }
+}
+}
