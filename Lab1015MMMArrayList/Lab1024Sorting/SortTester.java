82d12cf8567b5b3a546068ff50d1a2c5bc4c2df0
Roxy Reid <roxy@Silver-Surfer.local>
2019-10-21 21:07:14 -0700

    sort tester

--- Lab1015MMMArrayList/Lab1024Sorting/SortTester.java
+++ Lab1015MMMArrayList/Lab1024Sorting/SortTester.java
@@ -6,19 +6,23 @@
  * @version (1024)
  */
 import java.util.*;
+import java.util.Random;
 public class SortTester
 {
     // instance variables - replace the example below with your own
     ArrayList<Integer> numList = new ArrayList<Integer>();
-    
-
     /**
      * Constructor for objects of class SortTester
      */
     public SortTester()
     {
         // initialise instance variables
-        
+        loadNumbers(30);
+        printList("random");
+        bubbleSort();
+        printList("bubbleSort");
+        insertSort();
+        printList("insertSort");
     }
 
     /**
@@ -29,29 +33,48 @@ public class SortTester
      */
     public void loadNumbers(int n)
     {
-        // put your code here
+        // load array with random numbers 1-100 inclusive
+        Random rand = new Random();
+        for(int i = 0; i < n ; i++){
+            int rand_int = rand.nextInt(100)+ 1;
+            // numList.set(i, rand_int);
+            numList.add(rand_int);
+        }
     }
     
     public void bubbleSort(){
-        for(int i = nums.length-1; i > 0; i--){
+        for(int i = numList.size() - 1; i > 0; i--){
             for(int j = 0; j < i; j++){
-                if(nums[j] > nums[j+1]){
-                    swap (nums, j, j+1);
+                if(numList.get(j) > numList.get(j + 1)){
+                    Collections.swap(numList, j, j+1);
                 }
             }
         }
     }
     
     public void insertSort(){
-        for(int i = 1; i < arr.length; i++){
-            for(int j = 1; j > 0; j--){
-                if(numList.get(j) > numList.get(j-1)){
-                int temp = numList.get(j);
-                numList.set(j, j-1);
-                numList.set(j-1, temp);
-            }
+        for(int i = 1; i < numList.size(); i++){
+            if(numList.get(i) < numList.get(i-1)){
+                int temp = numList.get(i);
+                for(int j = i; j > 0; j--){
+                    int num = numList.get(j);
+                    if( j == 0 ){
+                        numList.set(j, temp);
+                    } else {
+                        numList.set(j, num);
+                    }
+                }
+            }   
         }
     }
+
+   
+   public void printList(String title){
+       System.out.print(title + ": ");
+       for(int i = 0; i < numList.size(); i++){
+           System.out.print(numList.get(i) + " ");
+        }
+        System.out.println();
     }
 }
 
